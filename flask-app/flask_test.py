fac70793c5db7018c34b68b120370c89f85d4b1a
Tyler Sengia <tylersengia@gmail.com>
2022-04-10 06:32:22 +0000

    Fixing form

--- flask-app/flask_test.py
+++ flask-app/flask_test.py
@@ -7,27 +7,24 @@ app.config['MYSQL_HOST'] = input("Enter MySQL hostname: ")
 app.config['MYSQL_USER'] = input("Enter username: ")
 app.config['MYSQL_PASSWORD'] = input("Enter password: ")
 app.config['MYSQL_DB'] = "sauron-db-dev1"
- 
-mysql = MySQL(app) 
 
-@app.route('/form')
-def form():
-	return render_template('form.html')
- 
-@app.route('/login', methods = ['POST', 'GET'])
+mysql = MySQL(app)
+
+@app.route('/', methods = ['GET', 'POST'])
 def login():
 	if request.method == 'GET':
-		return "Login via the login Form"
-     
+		return render_template('form.html')
+
 	if request.method == 'POST':
-		type = request.form['type']
-		latitude = request.form['latitude']
-		longitude = request.form['longitude']
-		description = request.form['description']        
+		print(request.form)
+		reportType = request.form['type']
+		latitude = float(request.form['latitude'])
+		longitude = float(request.form['longitude'])
+		description = request.form['description']
 		cursor = mysql.connection.cursor()
-		cursor.execute('''INSERT INTO report_list (type, latitude, longitude, description) VALUES(%s,%s,%s,%s)''',(type,latitude,longitude,description))
+		cursor.execute('''INSERT INTO report_list (type, latitude, longitude, description) VALUES(%s,%s,%s,%s)''',(reportType,latitude,longitude,description))
 		mysql.connection.commit()
 		cursor.close()
-		return "Done!"
+		return "Submitted!"
 
 app.run(host='0.0.0.0', port=5000)
