7ff60aae741dcac10027996c226503a67ca5451c
Tyler Sengia <tylersengia@gmail.com>
2022-04-10 08:03:12 +0000

    Added image plugin

--- packages.sh
+++ packages.sh
@@ -1,10 +1,11 @@
 
 # Install apt packages we need
-sudo apt-get install -y adduser libfontconfig1 wget default-mysql-client python3-pip python3-dev default-libmysqlclient-dev build-essential ffmpeg
+sudo apt-get install -y adduser libfontconfig1 wget default-mysql-client python3-pip python3-dev default-libmysqlclient-dev build-essential ffmpeg unzip
 
-do curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+# Install youtube-dl
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/bin/youtube-dl
+chmod a+rx /usr/bin/youtube-dl
 
-sudo chmod a+rx /usr/local/bin/youtube-dl
 # Upgrade pip
 pip install --upgrade pip setuptools wheel
 
@@ -21,9 +22,16 @@ dpkg -i grafana-enterprise_8.4.5_amd64.deb
 rm grafana-enterprise_8.4.5_amd64.deb
 
 # grab the dynamic image plugin
-wget https://grafana.com/api/plugins/dalvany-image-panel/versions/2.4.0/download
+curl -L https://grafana.com/api/plugins/dalvany-image-panel/versions/2.4.0/download -o dalvany-image-panel.zip
 
-# unzip it
+# Create the plugins directory b/c it doesn't exist by default
+mkdir -p /var/lib/grafana/plugins
+
+# unzip the plugin into the directory
+unzip dalvany-image-panel.zip -d /var/lib/grafana/plugins/dalvany-image-panel
+
+# Clean up
+rm dalvany-image-panel.zip
 
 # Enable Grafana on startup
 systemctl enable grafana-server.service
