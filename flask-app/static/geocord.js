6eaaa1972d7a33de6188d53530f66bcf50727a03
Tyler Sengia <tylersengia@gmail.com>
2022-04-10 08:18:15 +0000

    Adding geolocation grabbing

---
+++ flask-app/static/geocord.js
@@ -0,0 +1,18 @@
+function fillPosition(position_event) {
+	var lat_input = document.getElementById("lat-input");
+	var lon_input = document.getElementById("lon-input");
+
+	lat_input.value = position_event.coords.latitude;
+	lon_input.value = position_event.coords.latitude;
+}
+
+function checkLocation() {
+	if (navigator.geolocation) {
+		navigation.geolocation.getCurrentPosition(fillPosition);
+	}	
+	else {
+		console.log("Unable to grab geolocation.");
+	}
+}
+
+document.body.onload = checkLocation;
