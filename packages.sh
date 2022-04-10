6dec2fbece1dd5e9cd6b1366b2f2a4469a3c3cb3
Tyler Sengia <tylersengia@gmail.com>
2022-04-10 07:04:50 +0000

    Adding dynamic image plugin

--- packages.sh
+++ packages.sh
@@ -1,8 +1,25 @@
 # Install apt packages we need
-apt-get install -y wget default-mysql-client python-pip
+apt-get install -y adduser libfontconfig1 wget default-mysql-client python3-pip python3-dev default-libmysqlclient-dev build-essential
+
+# Upgrade pip
+pip install --upgrade pip setuptools wheel
 
 # Install pip packages
 pip3 install -r requirements.txt
+
+# Grab Grafana
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_8.4.5_amd64.deb
+
+# Install Grafana
+dpkg -i grafana-enterprise_8.4.5_amd64.deb
+
+# Delete old deb
+rm grafana-enterprise_8.4.5_amd64.deb
+
+# grab the dynamic image plugin
+wget https://grafana.com/api/plugins/dalvany-image-panel/versions/2.4.0/download
+
+# unzip it
 
 # Enable Grafana on startup
 systemctl enable grafana-server.service
